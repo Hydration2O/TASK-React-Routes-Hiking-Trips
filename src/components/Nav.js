ac1ff65e8fdecfe8197b3774e17d8a8cab89b78c
Hydration2O <bomalk121@gmail.com>
2024-11-19 18:27:50 +0300

    router+navBar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 function Nav() {
   return (
@@ -7,21 +8,51 @@ function Nav() {
       id="mainNav"
     >
       <div className="container">
-        <a className="navbar-brand" href="#">
+        <NavLink
+          className="navbar-brand"
+          to="/"
+          style={({ isActive, isPending, isTransitioning }) => {
+            return {
+              fontWeight: isActive ? "underline" : "",
+              color: isPending ? "red" : "black",
+              viewTransitionName: isTransitioning ? "slide" : "",
+            };
+          }}
+        >
           Hike
-        </a>
+        </NavLink>
 
         <div className="collapse navbar-collapse" id="navbarResponsive">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item mx-0 mx-lg-1">
-              <a className="nav-link py-3 px-0 px-lg-3 rounded" href="#">
+              <NavLink
+                className="nav-link py-3 px-0 px-lg-3 rounded"
+                to="/"
+                style={({ isActive, isPending, isTransitioning }) => {
+                  return {
+                    fontWeight: isActive ? "underline" : "",
+                    color: isPending ? "red" : "black",
+                    viewTransitionName: isTransitioning ? "slide" : "",
+                  };
+                }}
+              >
                 Home
-              </a>
+              </NavLink>
             </li>
             <li className="nav-item mx-0 mx-lg-1">
-              <a className="nav-link py-3 px-0 px-lg-3 rounded" href="#">
+              <NavLink
+                className="nav-link py-3 px-0 px-lg-3 rounded"
+                to="/TripsList"
+                style={({ isActive, isPending, isTransitioning }) => {
+                  return {
+                    fontWeight: isActive ? "underline" : "",
+                    color: isPending ? "red" : "black",
+                    viewTransitionName: isTransitioning ? "slide" : "",
+                  };
+                }}
+              >
                 Trips
-              </a>
+              </NavLink>
             </li>
           </ul>
         </div>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { RouterProvider } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./components/Home";
+import TripsList from "./components/TripsList";
+import TripDetail from "./components/TripDetail";
+import Nav from "./components/Nav";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <Nav />
+        <Home />
+      </div>
+    ),
+  },
+  {
+    path: "/Tripslist",
+    element: (
+      <div>
+        <Nav />
+        <TripsList />
+      </div>
+    ),
+  },
+  {
+    path: "/TripDetail",
+    element: (
+      <div>
+        <Nav />
+        <TripDetail />,
+      </div>
+    ),
+  },
+]);
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
